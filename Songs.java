0eb86397b7c0c1bc039f9e1eb2f0a6c33cf64c96
ellayp <ella.yap26@ycdsbk12.ca>
2025-04-30 23:59:57 -0400

    Created getter methods for Songs.java

--- Songs.java
+++ Songs.java
@@ -1,3 +1,35 @@
 public class Songs {
-    
+    private String title;
+    private String artist;
+    private String album;
+    private int releaseYear;
+    private int duration; // in seconds
+
+    public Songs(String title, String artist, String album, int releaseYear, int duration) {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.releaseYear = releaseYear;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public int getReleaseYear () {
+        return releaseYear;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
