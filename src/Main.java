00c4bb7e46705451fbe4481f645c4bdde356be92
ellayp <ella.yap26@ycdsbk12.ca>
2025-05-11 12:35:51 -0400

    Added the ability to add songs to user's collection

--- src/Main.java
+++ src/Main.java
@@ -18,60 +18,52 @@ public class Main extends ReadCSV {
                 System.out.println();
             }
 
+            
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter a genre to filter songs:");
-            String genre = scanner.nextLine();
+            System.out.println("Would you like to filter songs by genre? (yes/no)");
+            if (scanner.nextLine().equalsIgnoreCase("yes")) {
+                //Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter a genre to filter songs:");
+                String genre = scanner.nextLine();
 
-            List<Songs> filteredSongs = new ArrayList<>();
-            for (Songs song : songs) {
-                if (song.getGenre().equalsIgnoreCase(genre)) {
-                    filteredSongs.add(song);
+                List<Songs> filteredSongs = new ArrayList<>();
+                for (Songs song : songs) {
+                    if (song.getGenre().equalsIgnoreCase(genre)) {
+                        filteredSongs.add(song);
+                    }
                 }
-            }
-            scanner.close();
-            System.out.println();
-            System.out.println("Filtered Songs:");
-            for (Songs song : filteredSongs) {
-                System.out.println(song.getTitle() + " by " + song.getArtist());
+                System.out.println();
+                System.out.println("Filtered Songs:");
+                for (Songs song : filteredSongs) {
+                    System.out.println(song.getTitle() + " by " + song.getArtist());
+                }
+            } 
+            
+            System.out.println("Would you like to add a song to your collection? (yes/no)");
+            if (scanner.nextLine().equalsIgnoreCase("yes")) {
+                //Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter the title of the song:");
+                String title = scanner.nextLine();
+                System.out.println("Enter the artist of the song:");
+                String artist = scanner.nextLine();
+                System.out.println("Enter the album of the song:");
+                String album = scanner.nextLine();
+                System.out.println("Enter the release year of the song:");
+                int releaseYear = Integer.parseInt(scanner.nextLine());
+                System.out.println("Enter the duration of the song in seconds:");
+                int duration = Integer.parseInt(scanner.nextLine());
+                System.out.println("Enter the genre of the song:");
+                String genre = scanner.nextLine();
+
+                Songs newSong = new Songs(title, artist, album, releaseYear, duration, genre);
+                songs.add(newSong);
+                System.out.println("Thanks! Song added.");
+                scanner.close();
             }
         }
         catch (IOException e) {
             System.out.println("Error. Please try again. " + e.getMessage());
         }
-        
     }
+    
 }
-// import java.io.BufferedReader;
-// import java.io.FileReader;
-// import java.io.IOException;
-// import java.util.*;
-
-// public class Main {
-//     public static List<Songs> readSongs() throws IOException {
-//         BufferedReader songscsv = new BufferedReader(new FileReader("src/songs.csv"));
-//         songscsv.readLine(); // Skip header line
-//         String line;
-//         List<Songs> songs = new ArrayList<>();
-//         while ((line = songscsv.readLine()) != null) {
-//             String[] fields = line.split(",");
-//             String title = fields[0];
-//             String artist = fields[1];
-//             String album = fields[2];
-//             int releaseYear = Integer.parseInt(fields[3]);
-//             int duration = Integer.parseInt(fields[4]);
-//             String genre = fields[5];
-
-//             Songs song = new Songs(title, artist, album, releaseYear, duration, genre);
-//             songs.add(song);
-//         }
-//         songscsv.close();
-//         return songs;
-//     }
-
-
-
-//     public static void main(String[] args) throws IOException {
-//         List<Songs> songs = readSongs();
-//         System.out.println("Number of songs: " + songs.size());
-//     }
-// }
